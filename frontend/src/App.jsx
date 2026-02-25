f005aee1c7f2492529701630bc5bb057b51a0661
fykri <dzulfikriyunus@gmail.com>
2026-02-25 10:17:32 +0800

    login: ui frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
-import './index.css'
-
+import "./index.css";
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/Login";
 function App() {
-  return (
-    <>
-    </>
-  )
+    return (
+        <Routes>
+            <Route path="/" element={<LoginPage />} />
+        </Routes>
+    );
 }
 
-export default App
+export default App;
