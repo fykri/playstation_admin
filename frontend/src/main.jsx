3df62c8e40d3259b954f6f81779e0eb31648ef17
fykri <dzulfikriyunus@gmail.com>
2026-03-11 11:41:17 +0800

    setup: cakra ui

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/authContext.jsx";
+import { Provider } from "@/components/ui/provider";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <BrowserRouter>
             <AuthProvider>
-                <App />
+                <Provider>
+                    <App />
+                </Provider>
             </AuthProvider>
         </BrowserRouter>
     </StrictMode>,
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,17 +1,13 @@
-import { Box, Button } from "@mui/material";
-import AccountCircle from "@mui/icons-material/AccountCircle";
-import LockIcon from "@mui/icons-material/Lock";
-import FormTextField from "../component/inputs/FormTextField";
-import PasswordField from "../component/inputs/PasswordField";
 import { useContext, useState } from "react";
 import { login } from "../api/authApi";
 import { AuthContext } from "../context/authContext";
 import { useNavigate } from "react-router-dom";
+import { Button, HStack } from "@chakra-ui/react";
 
 const LoginPage = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    const [isError, setIsError] = useState("");
+    //const [isError, setIsError] = useState("");
     const { setAccessToken } = useContext(AuthContext);
 
     const navigate = useNavigate();
@@ -19,7 +15,7 @@ const LoginPage = () => {
         try {
             const result = await login(username, password);
             setAccessToken(result.accessToken);
-            navigate('/dashboard')
+            navigate("/dashboard");
         } catch (error) {
             setIsError(error.response.data.message);
         }
@@ -29,65 +25,13 @@ const LoginPage = () => {
         <div className="flex justify-center items-center min-w-77 sm:w-86 py-4 rounded-sm border-white border ">
             <div className="w-[90%] flex  items-center flex-col">
                 <header className="bg-white p-1.5 rounded-t-xl rounded-br-2xl">
-                    <LockIcon fontSize="large"></LockIcon>
+                    {/*<LockIcon fontSize="large"></LockIcon>*/}
+                    <HStack>
+                        <Button>Click me</Button>
+                        <Button>Click me</Button>
+                    </HStack>
                 </header>
-                <form className="mt-5">
-                    {/* FORM USERNAME */}
-                    <Box
-                        sx={{
-                            display: "flex",
-                            alignItems: "flex-end",
-                            width: 270,
-                        }}
-                    >
-                        <AccountCircle
-                            sx={{ color: "white", mr: 1, fontSize: 45 }}
-                        />
-                        <FormTextField
-                            label="Username"
-                            onChange={(e) => {
-                                setUsername(e.target.value);
-                            }}
-                        />
-                    </Box>
-                    <Box
-                        sx={{
-                            display: "flex",
-                            alignItems: "flex-end",
-                            width: 270,
-                            mt: 3,
-                        }}
-                    >
-                        <LockIcon
-                            sx={{ color: "white", mr: 1, fontSize: 45 }}
-                        />
-                        <PasswordField
-                            label="Password"
-                            onChange={(e) => {
-                                setPassword(e.target.value);
-                            }}
-                        />
-                    </Box>
-                    <Button
-                        variant="contained"
-                        fullWidth
-                        onClick={handleLogin}
-                        sx={{
-                            mt: 4,
-                            height: 45,
-                            borderRadius: 2,
-                            fontWeight: "bold",
-                            fontSize: 16,
-                            textTransform: "none",
-                            backgroundColor: "#1976d2",
-                            "&:hover": {
-                                backgroundColor: "#1565c0",
-                            },
-                        }}
-                    >
-                        Login
-                    </Button>
-                </form>
+                <form className="mt-5">{/* FORM USERNAME */}</form>
             </div>
         </div>
     );
