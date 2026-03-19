7e59385e30ed4421cbb09c30288a4c91ba4b409f
fykri <dzulfikriyunus@gmail.com>
2026-03-19 10:50:50 +0800

    delete: box in navbarlayout

--- frontend/src/layout/NavbarLayout.jsx
+++ frontend/src/layout/NavbarLayout.jsx
@@ -1,4 +1,4 @@
-import { Flex, Box } from "@chakra-ui/react";
+import { Flex} from "@chakra-ui/react";
 import Sidebar from "../components/navbar/SideBar";
 
 const NavbarLayout = ({ children }) => {
