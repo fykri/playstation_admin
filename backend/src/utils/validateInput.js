57314752732849334416c6d1a84ac08533c93a25
fykri <dzulfikriyunus@gmail.com>
2026-07-10 20:36:29 +0800

    backend bug: fixed throwStatus parameter

--- backend/src/utils/validateInput.js
+++ backend/src/utils/validateInput.js
@@ -38,7 +38,7 @@ const isValidDate = (dateString, fieldName = 'Date') => {
     const regex = /^\d{4}-\d{2}-\d{2}$/;
 
     if (!regex.test(dateString)) {
-        throwStatus(400, `${fieldName} tidak valid`);
+        throwStatus(`${fieldName} tidak valid`, 400);
     }
 
     const [year, month, day] = dateString.split('-').map(Number);
@@ -48,8 +48,36 @@ const isValidDate = (dateString, fieldName = 'Date') => {
     const valid = date.getFullYear() === year && date.getMonth() === month - 1 && date.getDate() === day;
 
     if (!valid) {
-        throwStatus(400, `${fieldName} tidak valid`);
+        throwStatus(`${fieldName} tidak valid`, 400);
     }
 };
 
-module.exports = { validateInputNumber, validateNotEmpty, isUUID, isValidDate };
+const validatePhoneNumber = phoneNumber => {
+    if (!phoneNumber) {
+        throwStatus('Nomor HP wajib diisi', 400);
+    }
+
+    const cleanedPhone = phoneNumber.replace(/\D/g, '');
+
+    if (!/^08\d{8,13}$/.test(cleanedPhone)) {
+        throwStatus('Nomor HP tidak valid', 400);
+    }
+
+    return cleanedPhone;
+};
+
+const validateTime = time => {
+    if (!time) {
+        throwStatus('Jam wajib diisi', 400);
+    }
+
+    const regex = /^([01]\d|2[0-3]):([0-5]\d)$/;
+
+    if (!regex.test(time)) {
+        throwStatus('Format jam tidak valid', 400);
+    }
+
+    return time;
+};
+
+module.exports = { validateInputNumber, validateNotEmpty, isUUID, isValidDate, validatePhoneNumber, validateTime };
