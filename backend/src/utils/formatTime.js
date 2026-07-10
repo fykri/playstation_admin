df6af0299bff0e1fe60fb7e50ba959bad7f55d33
fykri <dzulfikriyunus@gmail.com>
2026-07-10 20:35:07 +0800

    backend: add utils formatTime.js

---
+++ backend/src/utils/formatTime.js
@@ -0,0 +1,7 @@
+const formatHours = date => {
+    const hours = new Date(date).getHours().toString().padStart(2, '0');
+    const minutes = new Date(date).getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+};
+
+module.exports = { formatHours };
