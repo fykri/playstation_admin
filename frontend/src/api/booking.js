f79d6572148bb6c8b9fc02cd0f7255137dffd9f5
fykri <dzulfikriyunus@gmail.com>
2026-07-10 20:37:32 +0800

    frontend: add booking api

---
+++ frontend/src/api/booking.js
@@ -0,0 +1,91 @@
+import api from './axios';
+export const postBooking = async data => {
+    try {
+        const result = await api.post('/booking', data);
+        return result.data;
+    } catch (error) {
+        if (error.response && error.response.data) {
+            throw error.response.data || 'Terjadi kesalahan';
+        } else {
+            throw error;
+        }
+    }
+};
+
+export const getBookingActive = async () => {
+    try {
+        const result = await api.get('/booking/booking-active');
+        return result.data;
+    } catch (error) {
+        if (error.response && error.response.data) {
+            throw error.response.data || 'Terjadi kesalahan';
+        } else {
+            throw error;
+        }
+    }
+};
+
+export const getBookingTableActive = async () => {
+    try {
+        const result = await api.get('/booking/booking-table-active');
+        return result.data;
+    } catch (error) {
+        if (error.response && error.response.data) {
+            throw error.response.data || 'Terjadi kesalahan';
+        } else {
+            throw error;
+        }
+    }
+};
+
+export const getBookingById = async id_booking => {
+    try {
+        const result = await api.get(`/booking/booking-detail/${id_booking}`);
+        return result.data;
+    } catch (error) {
+        if (error.response && error.response.data) {
+            throw error.response.data || 'Terjadi kesalahan';
+        } else {
+            throw error;
+        }
+    }
+};
+
+export const updateBookingExpired = async id_booking => {
+    try {
+        const result = await api.patch(`/booking/expired/${id_booking}`);
+        return result.data;
+    } catch (error) {
+        if (error.response && error.response.data) {
+            throw error.response.data || 'Terjadi kesalahan';
+        } else {
+            throw error;
+        }
+    }
+};
+
+export const updateDataBooking = async (id_booking, data) => {
+    try {
+        const result = await api.patch(`/booking/${id_booking}`, data);
+        return result.data;
+    } catch (error) {
+        if (error.response && error.response.data) {
+            throw error.response.data || 'Terjadi kesalahan';
+        } else {
+            throw error;
+        }
+    }
+};
+
+export const cancelBooking = async id_booking => {
+    try {
+        const result = await api.delete(`/booking/${id_booking}`);
+        return result.data;
+    } catch (error) {
+        if (error.response && error.response.data) {
+            throw error.response.data || 'Terjadi kesalahan';
+        } else {
+            throw error;
+        }
+    }
+};
