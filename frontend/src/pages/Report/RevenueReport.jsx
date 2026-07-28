5db35b9078b82f4cf29ba4f67861ce7dd738f7d7
fykri <dzulfikriyunus@gmail.com>
2026-07-28 17:19:37 +0800

    FE: delete comment

--- frontend/src/pages/Report/RevenueReport.jsx
+++ frontend/src/pages/Report/RevenueReport.jsx
@@ -1,28 +1,6 @@
 import { Box, Text, HStack, Button } from '@chakra-ui/react';
 import RevenueReportComponent from './components/RevenueCard';
 import { useReportStore } from '@/stores/useReportStore';
-
-//    {
-//        periode: 'today',
-//        title: 'Hari ini',
-//    },
-//    {
-//        periode: 'week',
-//        title: 'Minggu Ini',
-//    },
-//    {
-//        periode: 'month',
-//        title: 'Bulan Ini',
-//    },
-//    {
-//        periode: 'year',
-//        title: 'Tahun Ini',
-//    },
-//    {
-//        periode: 'costum',
-//        title: 'Rentang Tanggal',
-//    },
-//];
 const RevenueReport = () => {
     const itemsRevenue = useReportStore(state => state.itemsRevenue);
 
